4dcf8b8d1d655a93054f4f08d4e8976da99f73cb
Patrik Hillbo <traddles@gmail.com>
2015-02-09 14:56:02 +0100

    One step to us flask and eve at same time

--- run.py
+++ run.py
@@ -1,5 +1,27 @@
 from eve import Eve
-app = Eve()
+from flask import Flask, url_for, session, g, redirect, request, flash, render_template, send_from_directory
+from flask.ext.mongoengine import MongoEngine, MongoEngineSessionInterface
 
+# APP
+#######
+# Eve
+app = Eve()
+# Flask
+flask = Flask(__name__)
+
+# DATABASE
+############
+# Database config
+flask.config["MONGODB_SETTINGS"] = {'DB': "eve"}
+#flask.config["MONGODB_DB"] = "flaskie"
+flask.config["SECRET_KEY"] = "M3\xbd\xe4\xa5 g\x13\x10\x98\xa8\xb3@\xb5z\xfd\x02J\x90\xfd\x9cC\x87\x11"
+# Database connection
+engine = MongoEngine(flask)
+client = engine.connection
+db = client.eve
+
+
+# MAIN
 if __name__ == '__main__':
-    app.run()
+	app.run(debug=True)
+    #flask.run(debug=True)
--- settings.py
+++ settings.py
@@ -1,1 +1,72 @@
-DOMAIN = {'unit': {}}
+
+
+
+# Enable reads (GET), inserts (POST) and DELETE for resources/collections
+# (if you omit this line, the API will default to ['GET'] and provide
+# read-only access to the endpoint).
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+
+# Enable reads (GET), edits (PATCH), replacements (PUT) and deletes of
+# individual items  (defaults to read-only item access).
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+schema = {
+    # Schema definition, based on Cerberus grammar. Check the Cerberus project
+    # (https://github.com/nicolaiarocci/cerberus) for details.
+    'name': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 15,
+        'required': True,
+        # talk about hard constraints! For the purpose of the demo
+        # 'lastname' is an API entry-point, so we need it to be unique.
+        'unique': True,
+    },
+    # 'role' is a list, and can only contain values from 'allowed'.
+    'state': {
+        'type': 'string',
+        'allowed': ["on", "off"],
+    },
+    # An embedded 'strongly-typed' dictionary.
+    'location': {
+        'type': 'dict',
+        'schema': {
+            'room': {'type': 'string'},
+            'level': {
+		        'type': 'string',
+		        'allowed': ["roof", "middle", "floor"],
+            }
+        },
+    },
+    'implemented': {
+        'type': 'datetime',
+    },
+}
+
+unit = {
+    # 'title' tag used in item links. Defaults to the resource title minus
+    # the final, plural 's' (works fine in most cases but not for 'people')
+    'item_title': 'unit',
+
+    # by default the standard item entry point is defined as
+    # '/people/<ObjectId>'. We leave it untouched, and we also enable an
+    # additional read-only entry point. This way consumers can also perform
+    # GET requests at '/people/<lastname>'.
+    'additional_lookup': {
+        'url': 'regex("[\w]+")',
+        'field': 'name'
+    },
+
+    # We choose to override global cache-control directives for this resource.
+    'cache_control': 'max-age=10,must-revalidate',
+    'cache_expires': 10,
+
+    # most global settings can be overridden at resource level
+    'resource_methods': ['GET', 'POST'],
+
+    'schema': schema
+}
+
+
+DOMAIN = {'unit': unit}
+
