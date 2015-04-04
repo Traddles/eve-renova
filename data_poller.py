8d7a6caf8061e1ad31a5cac0def73dc38449a4ed
Patrik Hillbo <traddles@gmail.com>
2015-04-04 22:34:53 +0200

    Data poller added

---
+++ data_poller.py
@@ -0,0 +1,35 @@
+from models import DB_PATH
+from random import uniform
+import time, dataset, datetime
+
+SIMUL = True
+
+if SIMUL:
+    print "Simulated"
+
+db = dataset.connect(DB_PATH)
+
+# get a reference to the table 'user'
+temp_table = db['ac_env_temp']
+
+polling_interval = 30*60.0 # (100 requests in 3600 seconds)
+
+poll_i = 0
+
+def poll_ac_env_temp_sensor():
+    global poll_i 
+    poll_i+=1
+    temp_sensor_value = uniform(8,10)
+    poll_i = temp_table.insert(dict(
+        poll_date=str(datetime.datetime.now()), 
+        data=temp_sensor_value,
+        simulated_data = SIMUL
+    ))
+    print ' >> poll no.', poll_i, "ac env temp: {:3.4f} degrees.".format(temp_sensor_value)
+
+running= True
+while running:
+    start = time.clock()
+    poll_ac_env_temp_sensor()
+    work_duration = time.clock() - start
+    time.sleep( polling_interval - work_duration )
